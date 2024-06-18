4272e4482e38fcbba7c6411d7b9fc5e2dfa1a1fa
Rashid Hussain <150262818+RH29152@users.noreply.github.com>
2024-06-18 13:55:35 +0500

    Update max_elements.cpp

--- max_elements.cpp
+++ max_elements.cpp
@@ -7,6 +7,11 @@ int printMax(int *arr,int i,int n){
     // return ((arr[i]<ans)?arr[i]:ans);
     return max(arr[i],printMax(arr,i+1,n));
 }
+//2nd method without index:
+int printMax(int *arr,int n){
+    if(i==1) return arr[0];
+    return max(arr[n-1],printMax(arr,n-2));
+}
 
 int main()
 {   int arr[5]={1,2,3,4,5};
