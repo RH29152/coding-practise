c99ec1f1dbf2ab9102a74fa02d45a997488d7b9f
Rashid Hussain <150262818+RH29152@users.noreply.github.com>
2024-06-19 12:48:24 +0500

    Update count7.cpp

--- count7.cpp
+++ count7.cpp
@@ -16,11 +16,12 @@ int count1(int n){
 
 //**********Effiecient recursive sol with TC=logN******** */
 
-int count2(int n,int count=0){
-    if(n==0) return count;
-    if(n%10==7) count++;
-    return count2(n/10,count); 
-} 
+int count2(int n){
+    if(n==0) return 0;
+    // if(n%10==7) { return (1+count2(n/10));}
+    // return count2(n/10);
+    return ((n%10==7)+count2(n/10));
+}  
 int main() {
     cout<<count2(1272737)<<endl;
     return 0;
